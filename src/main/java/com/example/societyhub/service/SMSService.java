67acfb81e713c76248a934431f1280d582c3a403
Gauri Desai <108411074+gaurid14@users.noreply.github.com>
2025-07-19 19:23:36 +0530

    Update SMSService.java

--- src/main/java/com/example/societyhub/service/SMSService.java
+++ src/main/java/com/example/societyhub/service/SMSService.java
@@ -11,15 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SMSService {
-    public static final String ACCOUNT_SID = "AC4be273cdde8f1c55ea9252c8ffef571e";
-    public static final String AUTH_TOKEN = "76e604b4a547215ba0167034597ec91c";
+    public static final String ACCOUNT_SID = "";
+    public static final String AUTH_TOKEN = "";
 
     @PostMapping("/sendSMS")
     public ResponseEntity<String> sendSMS(){
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
         Message message = Message.creator(
-                new com.twilio.type.PhoneNumber("+919769286834"),
-                "MGb7f123dbb9d67b0d5ddfcdd775aa1c28", "Hello user")
+                new com.twilio.type.PhoneNumber("+"),
+                "", "Hello user")
                 .create();
         return new ResponseEntity<>(HttpStatus.OK);
     }
